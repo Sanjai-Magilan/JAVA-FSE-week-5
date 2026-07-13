fef467470dc52c8da4c44eeeac9fa7e659e9ef3c
Sanjai-Magilan <sanjai6115@gmail.com>
2026-07-13 11:49:58 +0530

    Task 3 done in react

---
+++ scorecalculatorapp/src/App.js
@@ -0,0 +1,15 @@
+import { CalculateScore } from "./Components/CalculateScore";
+
+function App() {
+  return (
+    <div>
+    <CalculateScore Name ={"Steeve"}
+    School={"ABC Public School"}
+    total={284}
+    goal={3}
+    />
+    </div>
+  );
+}
+
+export default App;
